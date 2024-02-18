49dde21248559a8a5a68ae936ba5f46ddfb5d8c3
= <kilualthaus@hotmail.com>
2024-02-18 12:19:48 +0100

    aufgabe b und c Messaging

--- src/main/java/ch/hftm/boundary/MessagingTesting.java
+++ src/main/java/ch/hftm/boundary/MessagingTesting.java
@@ -18,7 +18,39 @@ public class MessagingTesting {
         return payload.toUpperCase();
     }
 
+//    @Incoming("processed-a")
+//    public void sink(String word) {
+//        System.out.println(">> " + word);
+//    }
+
+    // b) Weiterer Transformer einbinden
+    // Erstelle ein weiterer Transformer, welcher jeden Wert "BÖSE" mit "LIEBE" ersetzt. Stelle sicher,
+    // dass diese Transformation nach dem toUpperCase() stattfindet.
     @Incoming("processed-a")
+    @Outgoing("processed-b")
+    public String correctStrings(String payload) {
+        if(payload.equals("BÖSE")) {
+            return "LIEBE";
+        }
+        return payload;
+    }
+
+//    @Incoming("processed-b")
+//    public void sink(String word) {
+//        System.out.println(">> " + word);
+//    }
+
+    // c) Ergänze einen Filter
+    // Erweitere das Projekt mit einem zusätzlichem Transformer, welche die Zeichenketten ausschliesst,
+    // welche kleiner als 6 Zeichen sind. Verwende dazu ein Transformer, der mit Multi<String> arbeitet,
+    // als Input- sowie auch als Output-Typ
+    @Incoming("processed-b")
+    @Outgoing("processed-c")
+    public Multi<String> filter(Multi<String> input) {
+        return input.select().where(item -> item.length() > 5);
+    }
+
+    @Incoming("processed-c")
     public void sink(String word) {
         System.out.println(">> " + word);
     }
